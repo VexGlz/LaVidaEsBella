a0991a49f0703c3958823fe96d7e0124834d56f1
VexGlz <vexglz@gmail.com>
2025-11-29 00:19:18 -0600

    Main

---
+++ Negocio/src/main/java/error/negocio/Negocio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package error.negocio;
+
+/**
+ *
+ * @author Erick
+ */
+public class Negocio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
