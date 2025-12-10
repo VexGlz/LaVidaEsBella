be2a42abd1920623aa515c7e93862d5167fe56ef
AncorMx <angelcortez05@hotmail.com>
2025-12-10 15:47:05 -0700

    Modulos Casos Individuales Jose Luis

---
+++ CUBuscarMascotaIdeal/src/main/java/error/cubuscarmascotaideal/CUBuscarMascotaIdeal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package error.cubuscarmascotaideal;
+
+/**
+ *
+ * @author angel
+ */
+public class CUBuscarMascotaIdeal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
